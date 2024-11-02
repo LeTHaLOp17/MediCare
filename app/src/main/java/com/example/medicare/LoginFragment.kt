9c8da183d7b25a471cb5878c54c92b4ff816f3f9
Ayush Kumar Patel <aksingh6224@gmail.com>
2024-11-03 01:22:13 +0530

    Final design for login, signup, and home page

--- app/src/main/java/com/example/medicare/LoginFragment.kt
+++ app/src/main/java/com/example/medicare/LoginFragment.kt
@@ -1,23 +1,43 @@
 package com.example.medicare
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
 class LoginFragment : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.fragment_login)
         var signupBtn = findViewById<Button>(R.id.signup)
+        var loginBtn = findViewById<Button>(R.id.login)
+        var skipBtn = findViewById<TextView>(R.id.skipp)
 
         signupBtn.setOnClickListener {
             // Create an Intent to start the SignUp activity
             val intent = Intent(this@LoginFragment, SignUp::class.java)
+            // Start the SignUp activity
+            startActivity(intent)
+            finish()
+        }
+
+        loginBtn.setOnClickListener {
+            // Create an Intent to start the SignUp activity
+            val intent = Intent(this@LoginFragment, HomePage::class.java)
+            // Start the SignUp activity
+            startActivity(intent)
+            finish()
+        }
+        skipBtn.setOnClickListener {
+            // Create an Intent to start the SignUp activity
+            val intent = Intent(this@LoginFragment, HomePage::class.java)
             // Start the SignUp activity
             startActivity(intent)
             finish()
